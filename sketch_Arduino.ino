263d64271e1b2df080da95e31a08350d2938ad5e
Shiroko7 <thothctm@gmail.com>
2016-11-18 20:52:52 -0300

    Update sketch_Arduino.ino

--- sketch_Arduino.ino
+++ sketch_Arduino.ino
@@ -1,110 +1,64 @@
-int capSensePin = 2;
-int capSensePin2= 3;
-int capSensePin3= 4;
-int capSensePin4= 5;
-int capSensePin5= 6;
-int capSensePin6= 7;
-int capSensePin7= 8;
+/*
 
+En este proyecto usamos un piezoelectrico como sensor
 
+*/
 
-int touchedCutoff = 60;
+// declaramos las constantes para el uso de los pines
+const int pinPiezo = A0;
+const int pinPiezo1 = A1;
+const int pinPiezo2 = A2;
+const int pinPiezo3 = A3;
+const int pinPiezo4 = A4;
+const int pinPiezo5 =  A5;    // piezo conectado al pin analogico  0
 
-void setup(){
-Serial.begin(9600);
+// declaramos una constante con el valor del umbral
+const int umbral   = 10;    // valor umbral para detectar un golpe
+
+// declaramos las variables para los valores de lectura y estdo del led
+int lecturaSensor = 0;       // variable para gaurdar el valor del sensor
+int lecturaSensor1 = 0;
+int lecturaSensor2 = 0;
+int lecturaSensor3 = 0;
+int lecturaSensor4 = 0;
+int lecturaSensor5 = 0;
+
+void setup() {
+	Serial.begin(9600);        // inicializamos la comunicacion serial
 }
 
-void loop(){
-
-if (readCapacitivePin(capSensePin) > touchedCutoff) {
-Serial.print("TOCANDO1 \n");
-}
-if ( (millis() % 500) == 0){
-}
-
-///////////////////////
-if (readCapacitivePin(capSensePin2) > touchedCutoff) {
-Serial.print("TOCANDO2 \n");
-}
-if ( (millis() % 500) == 0){
-}
-
-if (readCapacitivePin(capSensePin3) > touchedCutoff){
-  Serial.print("TOCANDO3 \n");
-}
-if ( (millis() % 500) == 0){
-}
-
-if (readCapacitivePin(capSensePin4) > touchedCutoff){
-  Serial.print("TOCANDO4 \n");
-}
-if ( (millis() % 500) == 0){
-}
-
-if (readCapacitivePin(capSensePin5) > touchedCutoff){
-  Serial.print("TOCANDO5 \n");
-}
-if ( (millis() % 500) == 0){
-}
-
-if (readCapacitivePin(capSensePin6) > touchedCutoff){
-  Serial.print("TOCANDO6 \n");
-}
-if ( (millis() % 500) == 0){
-}
-
-if (readCapacitivePin(capSensePin7) > touchedCutoff){
-  Serial.print("TOCANDO7 \n");
-}
-
-// Every 500 ms, print the value of the capacitive sensor
-if ( (millis() % 500) == 0){
-}
-}
-
-
-uint8_t readCapacitivePin(int pinToMeasure){
-
-volatile uint8_t* port;
-volatile uint8_t* ddr;
-volatile uint8_t* pin;
-
-byte bitmask;
-if ((pinToMeasure >= 0) && (pinToMeasure <= 7)){
-port = &PORTD;
-ddr = &DDRD;
-bitmask = 1 << pinToMeasure;
-pin = &PIND;
-}
-if ((pinToMeasure > 7) && (pinToMeasure <= 13)){
-port = &PORTB;
-ddr = &DDRB;
-bitmask = 1 << (pinToMeasure - 8);
-pin = &PINB;
-}
-if ((pinToMeasure > 13) && (pinToMeasure <= 19)){
-port = &PORTC;
-ddr = &DDRC;
-bitmask = 1 << (pinToMeasure - 13);
-pin = &PINC;
-}
-
-*port &= ~(bitmask);
-*ddr  |= bitmask;
-delay(0);
-
-*ddr &= ~(bitmask);
-
-int cycles = 16000;
-for(int i = 0; i < cycles; i++){
-if (*pin & bitmask){
-cycles = i;
-break;
-}
-}
-
-*port &= ~(bitmask);
-*ddr  |= bitmask;
-
-return cycles;
+void loop() {
+	// leemos el valor del sensor en lecturaSensor
+	lecturaSensor = analogRead(pinPiezo);
+	lecturaSensor1 = analogRead(pinPiezo1);
+	lecturaSensor2 = analogRead(pinPiezo2);
+	lecturaSensor3 = analogRead(pinPiezo3);
+	lecturaSensor4 = analogRead(pinPiezo4);
+	lecturaSensor5 = analogRead(pinPiezo5);
+	// si el valor del sensor es mayor que el umbral
+	if (lecturaSensor >= umbral) {
+	// Y mandamos un "Golpe!!" a la computadora por el serial
+		Serial.print("TOCANDO1,");
+		Serial.println(lecturaSensor);
+	}
+	if (lecturaSensor1 >= umbral){
+		Serial.print("TOCANDO2,");
+		Serial.println(lecturaSensor1);
+	}
+	if (lecturaSensor2 >= umbral){
+		Serial.print("TOCANDO3,");
+		Serial.println(lecturaSensor2);
+	}
+	if (lecturaSensor3 >= umbral){
+		Serial.print("TOCANDO4,");
+		Serial.println(lecutasensor3);
+	}
+	if (lecturaSensor4 >= umbral){
+		Serial.print("TOCANDO5,");
+		Serial.println(lecturaSensor4);
+	}
+	if (lecturaSensor5 >= umbral){
+		Serial.print("TOCANDO6,");
+		Serial.println(lecturaSensor5);
+	}
 }
